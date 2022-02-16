5065347aec1f9bb7e16e2f2413771f48ddbb48d0
hammal <s19bsit010@student.edu.ug.pk>
2022-02-16 22:54:44 +0500

    about page

--- app.js
+++ app.js
@@ -9,6 +9,9 @@ app.get('/', (req, res) => {
 app.get('/about', (req, res) => {
     res.send('Hello World!')
   })
+  app.get('/contact', (req, res) => {
+    res.send('Hello World!')
+  })
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
